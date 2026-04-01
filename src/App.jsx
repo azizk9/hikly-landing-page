c449dbf868275145243cec2c21a7e9d77a8c5b0b
abdulaziz-kanjo <amer11993@hotmail.com>
2026-04-01 10:01:29 +0300

    Fix: correct case sensitivity in imports for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import NavBar from "./components/navbar/navbar";
+import NavBar from "./components/navbar/NavBar"; // التأكد من N و B كابيتال
 import Hero from "./components/hero/hero";
 import TripsSection from "./components/TripsSection/TripsSection";
 import AboutSection from "./components/AboutSection/AboutSection";
 import GallerySection from "./components/GallerySection/GallerySection";
 import BlogSection from "./components/blog/BlogSection";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/footer"; // التأكد من f سمول كما في مجلدك
 
 import footerData from "./data/footerData";
 import galleryData from "./data/galleryData";
 import aboutData from "./data/aboutData";
 import blogData from "./data/blogData";
 import tripsData from "./data/tripsData";
+import heroData from "./data/heroData";
+import navbarData from "./data/navbarData";
+
 function App() {
   return (
     <>
-      <NavBar />
-      <Hero />
+      <NavBar data={navbarData} />
+      <Hero data={heroData} />
       <AboutSection data={aboutData} />
-  <TripsSection data={tripsData} />
+      <TripsSection data={tripsData} />
       <GallerySection data={galleryData} />
       <BlogSection data={blogData} />
       <Footer data={footerData} />
